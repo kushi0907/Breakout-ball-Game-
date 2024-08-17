5f6f57ba1a256c50200e8b3851ad6736a729446b
Vikas Patel M A <121777934+kushi0907@users.noreply.github.com>
2024-08-17 20:01:07 +0530

    Add files via upload
    When the game is started, in order to move the ball, we have to detect if it’s touching left, right, or top of the panel and change its direction accordingly. Then, we have to find the collision between the ball and the paddle, and the ball and the map. For detecting collisions between two objects, we need a rectangle, so we have to create a rectangle around the ball as it is oval.

---
+++ action.java
@@ -0,0 +1,74 @@
+public void actionPerformed(ActionEvent e) {
+		
+		timer.start();
+		if(play) {
+						
+			//to move the ball when game is started
+			ballposX += ballXdir;
+			ballposY += ballYdir;
+			
+			if(ballposX < 0)
+			{
+				ballXdir = -ballXdir;
+			}
+			if(ballposY < 0)
+			{
+				ballYdir = -ballYdir;
+			}
+			if(ballposX > 670)
+			{
+				ballXdir = -ballXdir;
+			}
+			
+			//collision between ball and paddle
+			if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8)))
+			{
+				ballYdir = -ballYdir;
+			}
+			
+			// check map collision with the ball	
+			//here in map.map.length first map is object we have created and second map is the 2d array we have created
+
+			A: for(int i = 0; i<map.map.length; i++)
+			{
+				for(int j =0; j<map.map[0].length; j++)
+				{				
+					if(map.map[i][j] > 0)
+					{
+						//for intersection we need to first detect the position of ball and brick with respect to height and width of the brick
+						int brickX = j * map.brickWidth + 80;
+						int brickY = i * map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);					
+						Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect))
+						{					
+							map.setBrickValue(0, i, j);
+							score+=5;	
+							totalbricks--;
+							
+							// when ball hit right or left of brick
+							if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width)	
+							{
+								ballXdir = -ballXdir;
+							}
+							// when ball hits top or bottom of brick
+							else
+							{
+								ballYdir = -ballYdir;				
+							}
+							
+							break A;
+						}
+					}
+				}
+			}
+			
+			
+			
+			repaint();
+		}
